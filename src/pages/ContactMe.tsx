5ece3c5e68de5f8a82a2f1f05152b50e05141716
Ryan Coppa <ryan.tcoppa@gmail.com>
2023-02-03 02:00:49 -0500

    Contact me done and My Work started

--- src/pages/ContactMe.tsx
+++ src/pages/ContactMe.tsx
@@ -6,7 +6,7 @@ import { SocialIcon } from "react-social-icons";
 export const ContactMe = () => {
   return (
     <motion.div
-      className="flex h-screen items-center justify-center"
+      className="h-screen flex-1 items-center justify-center text-center"
       variants={pageAnimation}
       initial="hidden"
       animate="show"
@@ -14,14 +14,14 @@ export const ContactMe = () => {
     >
       <div>
         <div>
-          <motion.h2 variants={titleAnim}>
-            Contact <span>Me</span>
+          <motion.h2 className="py-10 text-3xl md:text-7xl md:py-20" variants={titleAnim}>
+            Contact <span className="text-cyan-custom">Me</span>
           </motion.h2>
         </div>
       </div>
       <div>
         <div>
-          <motion.div variants={titleAnim}>
+          <motion.div className="py-12" variants={titleAnim}>
             <a
               className="rounded-md border-2 border-solid border-cyan-custom px-8 py-2 text-lg transition ease-in-out hover:bg-cyan-custom md:py-4 md:text-xl"
               href={resume}
@@ -31,15 +31,15 @@ export const ContactMe = () => {
             </a>
           </motion.div>
         </div>
-        <div>
-          <motion.div variants={titleAnim}>
+        <div className="flex justify-center py-10 md:py-20">
+          <motion.div className="px-5" variants={titleAnim}>
             <SocialIcon
               url="https://www.linkedin.com/in/ryan-coppa"
               style={{ height: 80, width: 80 }}
               target="_blank"
             />
           </motion.div>
-          <motion.div>
+          <motion.div className="px-5" variants={titleAnim}>
             <SocialIcon
               url="https://github.com/ryanc268"
               style={{ height: 80, width: 80 }}
@@ -49,7 +49,7 @@ export const ContactMe = () => {
         </div>
         <div>
           <motion.div variants={titleAnim}>
-            <p>🚧 Page Still Under Construction 🚧</p>
+            <p className="pt-28">🚧 Page Still Under Construction 🚧</p>
           </motion.div>
         </div>
       </div>
--- src/pages/MyWork.tsx
+++ src/pages/MyWork.tsx
@@ -1,3 +1,129 @@
+//Images
+import card from "../assets/valerianX-card.jpg";
+import goonIcon from "../assets/goonbot-small.png";
+import reactbig from "../assets/react-icon-big.png";
+import cubegame from "../assets/cube-game.png";
+//Router
+import { Link } from "react-router-dom";
+//Animations
+import { motion } from "framer-motion";
+import {
+  pageAnimation,
+  fade,
+  photoAnim,
+  lineAnim,
+  slider,
+  sliderContainer,
+} from "../utils/Animation";
+//Components
+import { useScroll } from "../hooks/useScroll";
+import ScrollTop from "../utils/ScrollTop";
+
 export const MyWork = () => {
-  return <div></div>;
+  const isMobile = () => {
+    return window.innerWidth < 700;
+  };
+
+  const [element, controls] = useScroll(isMobile() ? 700 : 0);
+  const [element2, controls2] = useScroll(isMobile() ? 900 : 0);
+  const [element3, controls3] = useScroll(0);
+
+  return (
+    <motion.div
+      className="flex-1 text-center"
+      variants={pageAnimation}
+      initial="hidden"
+      animate="show"
+      exit="exit"
+      style={{ background: "#1b1b1b" }}
+    >
+      <motion.div variants={sliderContainer}>
+        <motion.div
+          className="fixed left-0 z-20 h-screen w-screen bg-indigo-900"
+          variants={slider}
+        ></motion.div>
+        <motion.div
+          className="fixed left-0 z-20 h-screen w-screen bg-cyan-600"
+          variants={slider}
+        ></motion.div>
+        <motion.div
+          className="fixed left-0 z-20 h-screen w-screen bg-violet-900"
+          variants={slider}
+        ></motion.div>
+        <motion.div
+          className="fixed left-0 z-20 h-screen w-screen bg-cyan-300"
+          variants={slider}
+        ></motion.div>
+      </motion.div>
+      <motion.div className="flex-1">
+        <motion.h2 className="text-2xl" variants={fade}>
+          <span className="text-cyan-custom">Valerian X</span> C# Mobile Game
+        </motion.h2>
+        <motion.div
+          className="h-0.5 bg-cyan-custom"
+          variants={lineAnim}
+        ></motion.div>
+        <Link to="/work/valerianX">
+          <div className="overflow-hidden">
+            <motion.img variants={photoAnim} src={card} alt="ValerianX Card" />
+          </div>
+        </Link>
+      </motion.div>
+      <motion.div
+        variants={fade}
+        animate={controls}
+        initial="hidden"
+        ref={element}
+      >
+        <motion.h2 className="text-2xl">
+          <span className="text-cyan-custom">Cube Game</span> Multiplayer
+          Typescript Party-Game using Websockets
+        </motion.h2>
+        <motion.div
+          className="h-0.5 bg-cyan-custom"
+          variants={lineAnim}
+        ></motion.div>
+        <a href="https://ryanc268-typescript-websocket-game.up.railway.app/">
+          <img src={cubegame} alt="Cube Game Banner" />
+        </a>
+      </motion.div>
+      <motion.div
+        variants={fade}
+        animate={controls2}
+        initial="hidden"
+        ref={element2}
+      >
+        <motion.h2 className="text-2xl">
+          React <span className="text-cyan-custom">Audio Library</span> w/
+          Visualization (Featured in Music Prod Tab)
+        </motion.h2>
+        <motion.div
+          className="h-0.5 bg-cyan-custom"
+          variants={lineAnim}
+        ></motion.div>
+        <Link to="/work/react-music-player">
+          <img src={reactbig} alt="React Icon" />
+        </Link>
+      </motion.div>
+      <motion.div
+        variants={fade}
+        animate={controls3}
+        initial="hidden"
+        ref={element3}
+      >
+        <motion.h2 className="text-2xl">
+          <span className="text-cyan-custom">Goon Bot</span> NodeJS Discord Bot
+          (Page Under Contruction)
+        </motion.h2>
+        <motion.div
+          className="h-0.5 bg-cyan-custom"
+          variants={lineAnim}
+        ></motion.div>
+        <Link className="flex justify-center" to="/work/goon-bot">
+          <img src={goonIcon} alt="Goon Bot Icon" />
+        </Link>
+      </motion.div>
+      <ScrollTop />
+    </motion.div>
+  );
 };
