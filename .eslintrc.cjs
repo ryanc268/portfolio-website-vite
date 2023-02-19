b2fd307c9bbc230fe68e9060248515f3e75047d8
Ryan Coppa <ryan.tcoppa@gmail.com>
2023-02-18 20:16:43 -0500

    changing eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,41 +1,26 @@
 module.exports = {
-  extends: [
-    // By extending from a plugin config, we can get recommended rules without having to add them manually.
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:import/recommended",
-    "plugin:@typescript-eslint/recommended",
-    // This disables the formatting rules in ESLint that Prettier is going to be responsible for handling.
-    // Make sure it's always the last config, so it gets the chance to override other configs.
-    "eslint-config-prettier",
-  ],
-  //env and overrides disable the module.exports error
-  env: {
-    es2022: true,
-  },
-    overrides: [
-    {
-      files: ['**/*.cjs'],
-      env: {
-        node: true,
-      },
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
     },
-  ],
-  settings: {
-    react: {
-      // Tells eslint-plugin-react to automatically detect the version of React to use.
-      version: "detect",
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
     },
-    // Tells eslint how to resolve imports
-    "import/resolver": {
-      node: {
-        paths: ["src"],
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
-      },
-    },
-  },
-  rules: {
-    // Add your own rules here to override ones from the extended configs.
-    "react/react-in-jsx-scope": "off",
-  },
-};
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "react/react-in-jsx-scope": "off",
+    }
+}
