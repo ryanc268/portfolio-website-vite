cbbaff537020d06e453b066606d1019aa841534f
Ryan Coppa <ryan.tcoppa@gmail.com>
2023-03-12 19:43:58 -0400

    Added AI Voice Convos project to My Work

--- src/pages/MyWork.tsx
+++ src/pages/MyWork.tsx
@@ -3,6 +3,7 @@ import card from "../assets/valerianX-card.jpg";
 import goonIcon from "../assets/goonbot-small.png";
 import visualizationBanner from "../assets/visualizer-banner.png";
 import cubegame from "../assets/cube-game.png";
+import aiVoiceBanner from "../assets/ai-voice-chat-banner.png";
 //Router
 import { Link } from "react-router-dom";
 //Animations
@@ -25,6 +26,7 @@ export const MyWork: React.FC = () => {
   const [element2, controls2] = useScroll(0);
   const [element3, controls3] = useScroll(0);
   const [element4, controls4] = useScroll(0);
+  const [element5, controls5] = useScroll(0);
 
   return (
     <motion.div
@@ -40,19 +42,19 @@ export const MyWork: React.FC = () => {
         <motion.div
           className="fixed left-0 top-0 z-20 h-screen w-screen bg-indigo-900"
           variants={slider}
-        ></motion.div>
+        />
         <motion.div
           className="fixed left-0 top-0 z-20 h-screen w-screen bg-cyan-600"
           variants={slider}
-        ></motion.div>
+        />
         <motion.div
           className="fixed left-0 top-0 z-20 h-screen w-screen bg-violet-900"
           variants={slider}
-        ></motion.div>
+        />
         <motion.div
           className="fixed left-0 top-0 z-20 h-screen w-screen bg-cyan-300"
           variants={slider}
-        ></motion.div>
+        />
       </motion.div>
       <motion.div
         className="px-10 py-4 md:w-2/5"
@@ -65,10 +67,7 @@ export const MyWork: React.FC = () => {
           <span className="text-cyan-custom">Valerian X</span> C# Mobile Game
           Made With Unity 2D
         </motion.h2>
-        <motion.div
-          className="h-0.5 bg-cyan-custom"
-          variants={lineAnim}
-        ></motion.div>
+        <motion.div className="h-0.5 bg-cyan-custom" variants={lineAnim} />
         <Link to="/work/valerianX">
           <div className="flex justify-center overflow-hidden">
             <motion.img
@@ -93,10 +92,7 @@ export const MyWork: React.FC = () => {
           <span className="text-cyan-custom">Cube Game</span> Multiplayer
           Typescript Party-Game using Websockets
         </motion.h2>
-        <motion.div
-          className="h-0.5 bg-cyan-custom"
-          variants={lineAnim}
-        ></motion.div>
+        <motion.div className="h-0.5 bg-cyan-custom" variants={lineAnim} />
         <a
           className=" flex justify-center"
           href="https://ryanc268-typescript-websocket-game.up.railway.app/"
@@ -124,10 +120,7 @@ export const MyWork: React.FC = () => {
           React <span className="text-cyan-custom">Audio Library</span> w/
           Visualization (Featured in Music Prod Tab)
         </motion.h2>
-        <motion.div
-          className="h-0.5 bg-cyan-custom"
-          variants={lineAnim}
-        ></motion.div>
+        <motion.div className="h-0.5 bg-cyan-custom" variants={lineAnim} />
         <Link className="flex justify-center" to="/music">
           <motion.img
             className="pt-4 md:w-full"
@@ -147,13 +140,38 @@ export const MyWork: React.FC = () => {
         ref={element4}
       >
         <motion.h2 className="py-4 text-2xl md:text-4xl">
+          <span className="text-cyan-custom">Voice Chat With AI </span>
+          Typescript full-stack project using the T3 stack
+        </motion.h2>
+        <motion.div className="h-0.5 bg-cyan-custom" variants={lineAnim} />
+        <a
+          className=" flex justify-center"
+          href="https://ai-voice-conversations-production.up.railway.app/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <motion.img
+            className="pt-4 md:w-full"
+            src={aiVoiceBanner}
+            alt="Goon Bot Icon"
+            variants={hoverShake}
+            initial="start"
+            whileHover="hover"
+          />
+        </a>
+      </motion.div>
+      <motion.div
+        className="px-10 py-4 md:w-2/5"
+        variants={fade}
+        animate={controls5}
+        initial="hidden"
+        ref={element5}
+      >
+        <motion.h2 className="py-4 text-2xl md:text-4xl">
           <span className="text-cyan-custom">Goon Bot</span> NodeJS Discord Bot
           (Page Under Contruction)
         </motion.h2>
-        <motion.div
-          className="h-0.5 bg-cyan-custom"
-          variants={lineAnim}
-        ></motion.div>
+        <motion.div className="h-0.5 bg-cyan-custom" variants={lineAnim} />
         <Link className="flex justify-center" to="/work/goon-bot">
           <motion.img
             className="pt-4 md:w-full"
